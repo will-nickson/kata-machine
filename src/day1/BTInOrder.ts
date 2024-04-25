5e947c4e10464ead18cd0d9a0e49d6f639869fd1
Will Nickson <wfgnickson@gmail.com>
2024-04-25 08:58:57 +0100

    implement binary tree in order

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,20 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
 
+    // recurse
+    // pre
+    walk(curr.left, path);
+
+    // recurse
+    path.push(curr.value);
+    walk(curr.right, path);
+
+    // post
+    return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
